99f3619a8719d08eed82f5697965577bf28b87e7
sria-the-fury <jakariamsria@gmail.com>
2022-07-09 16:00:57 +0600

    check mdi

--- lib/pages/Crime_Home.dart
+++ lib/pages/Crime_Home.dart
@@ -2,8 +2,8 @@ import 'package:crime_reporter/pages/Login.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class CrimeHome extends StatefulWidget {
   const CrimeHome({Key? key}) : super(key: key);
@@ -37,6 +37,12 @@ class _CrimeHomeState extends State<CrimeHome> {
                 )
               ],
             ),
-            backgroundColor: const Color(0xff161650)));
+            backgroundColor: const Color(0xff161650)),
+      body: Container(
+        alignment: Alignment.center,
+        child: const Icon(MdiIcons.sword, color: Colors.green, size: 500,),
+      )
+
+    );
   }
 }
